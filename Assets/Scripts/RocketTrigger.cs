23deda1dcce9ab17d05d65332e53454aa6c33ab6
LaKostyaLi <lakostyali1996@gmail.com>
2022-03-10 14:57:48 +0300

    I am for peace

---
+++ Assets/Scripts/RocketTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Rocket"))
+        {
+         Score._goal++;
+        }
+    }
+    
+}
