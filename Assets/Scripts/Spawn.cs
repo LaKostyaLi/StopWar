cb42e5c5537f7f91d4d5a8870dfa94667b306cf2
LaKostyaLi <lakostyali1996@gmail.com>
2022-03-11 13:04:32 +0300

    I am for peace

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //спаун без использования корутины
+    public GameObject _spawnObj;
+    public float time;
+    float lastSpawnedTime;
+
+
+    void Update()
+    {
+        if(Time.time>lastSpawnedTime+time)
+        { 
+          SpawnPref();
+          lastSpawnedTime = Time.time;
+        }
+    }
+
+    public void SpawnPref()
+    {
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-14, 20), 30, Random.Range(-11, 11));
+
+        Instantiate(_spawnObj, randomSpawnPosition, Quaternion.identity);
+    }
+   
+
+}
